f888513dabcdd4d10da3b93ce6d04bf643494bd4
S-Mardii <s.mardy009@gmail.com>
2017-04-29 22:23:22 +0700

    Initial commit

---
+++ app/src/main/java/com/camitss/user/prokids/activity/newGameActivity.java
@@ -0,0 +1,15 @@
+package com.camitss.user.prokids.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.camitss.user.prokids.R;
+
+public class newGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+}
